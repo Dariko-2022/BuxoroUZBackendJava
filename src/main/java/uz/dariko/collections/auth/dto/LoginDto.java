bce18ecd935a00070278ff56e27e24c259631c2d
NajmiddinIbrokhimoff <ibrokhimoff3105@gmail.com>
2022-10-11 17:09:05 +0500

    security quwilyapti

---
+++ src/main/java/uz/dariko/collections/auth/dto/LoginDto.java
@@ -0,0 +1,16 @@
+package uz.dariko.collections.auth.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+
+@Setter
+@Getter
+@AllArgsConstructor
+@NoArgsConstructor
+public class LoginDto {
+    private String username;
+    private String password;
+}
