8cf670ffc34a9bf8d8d6d90231c318e37e13dba4
NajmiddinIbrokhimoff <ibrokhimoff3105@gmail.com>
2022-10-13 17:08:41 +0500

    CorsConfig added

---
+++ src/main/java/uz/dariko/configs/CorsConfig.java
@@ -0,0 +1,20 @@
+package uz.dariko.configs;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+import java.util.Arrays;
+
+@Configuration
+public class CorsConfig implements WebMvcConfigurer {
+    // origin IP olmaydi.
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**").
+                allowCredentials(true).
+                allowedOrigins("http://localhost:3000/").
+                allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS", "HEAD", "PATCH").
+                allowedHeaders("*");
+    }
+}
