a328c5c32203ed316871b58125c51d3bb376ae91
Arslonboy <arslonnurobod99@gmail.com>
2022-10-19 12:38:35 +0500

    InfoGroup crud added

--- src/main/java/uz/dariko/collections/infoGroup/InfoGroupRepository.java
+++ src/main/java/uz/dariko/collections/infoGroup/InfoGroupRepository.java
@@ -1,11 +1,22 @@
 package uz.dariko.collections.infoGroup;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+import java.util.Optional;
 import java.util.UUID;
 
 
 @Repository
 public interface InfoGroupRepository extends JpaRepository<InfoGroup, UUID> {
+
+
+    @Query(nativeQuery = true,value = "SELECT * FROM infogroup where is_deleted = ?")
+    List<InfoGroup> findAllByDeleted(boolean deleted);
+
+
+    @Query(nativeQuery = true,value = "Select * from infogroup where id = ?1 and is_deleted = false")
+    Optional<InfoGroup> findByIdAndDeletedNot(UUID uuid);
 }
--- src/main/java/uz/dariko/collections/infoGroup/InfoGroupService.java
+++ src/main/java/uz/dariko/collections/infoGroup/InfoGroupService.java
@@ -3,6 +3,13 @@ package uz.dariko.collections.infoGroup;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 import uz.dariko.base.service.BaseService;
+import uz.dariko.collections.infoGroup.dto.InfoGroupCreateDTO;
+import uz.dariko.collections.infoGroup.dto.InfoGroupDTO;
+import uz.dariko.collections.infoGroup.dto.InfoGroupUpdateDTO;
+import uz.dariko.utils.EntityGetter;
+
+import java.util.List;
+import java.util.UUID;
 
 @Service
 public class InfoGroupService implements BaseService {
@@ -11,12 +18,54 @@ public class InfoGroupService implements BaseService {
 
     private final InfoGroupMapper infoGroupMapper;
 
+    private final EntityGetter entityGetter;
 
 
-    public InfoGroupService(InfoGroupRepository infoGroupRepository, InfoGroupMapper infoGroupMapper) {
+
+    public InfoGroupService(InfoGroupRepository infoGroupRepository, InfoGroupMapper infoGroupMapper, EntityGetter entityGetter) {
         this.infoGroupRepository = infoGroupRepository;
         this.infoGroupMapper = infoGroupMapper;
+        this.entityGetter = entityGetter;
     }
 
-//    public ResponseEntity<?> create()
+    public ResponseEntity<?> create(InfoGroupCreateDTO infoGroupCreateDto) {
+        InfoGroup infoGroup = infoGroupMapper.fromCreateDto(infoGroupCreateDto);
+
+        infoGroupRepository.save(infoGroup);
+        return ResponseEntity.status(201).body("saved");
+    }
+
+    public ResponseEntity<?> getAll() {
+        List<InfoGroup> all = infoGroupRepository.findAllByDeleted(false);
+        List<InfoGroupDTO> govSphereDTOS = infoGroupMapper.toDtoList(all);
+        return ResponseEntity.ok(govSphereDTOS);
+    }
+
+    public ResponseEntity<?> update(InfoGroupUpdateDTO dto){
+
+        InfoGroup infoGroup = entityGetter.getInfoGroup(dto.getId());
+        infoGroup.setUzName(dto.getUzName());
+        infoGroup.setKrName(dto.getKrName());
+        infoGroup.setRuName(dto.getRuName());
+        InfoGroup save = infoGroupRepository.save(infoGroup);
+        InfoGroupDTO infoGroupDTO = infoGroupMapper.toDto(save);
+        return ResponseEntity.ok(infoGroupDTO);
+
+    }
+
+    public ResponseEntity<?> get(UUID id) {
+        InfoGroup infoGroup = entityGetter.getInfoGroup(id);
+        InfoGroupDTO infoGroupDTO = infoGroupMapper.toDto(infoGroup);
+        return ResponseEntity.ok(infoGroupDTO);
+    }
+
+    public ResponseEntity<?> delete(UUID id) {
+        InfoGroup infoGroup = entityGetter.getInfoGroup(id);
+        infoGroup.setDeleted(true);
+        infoGroupRepository.save(infoGroup);
+        return ResponseEntity.ok(true);
+
+    }
+
+
 }
